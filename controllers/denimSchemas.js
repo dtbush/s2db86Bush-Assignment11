e3c4a3d94db831d0c7fdc5271990bac715232cae
Drake Bush <54418983+dtbush@users.noreply.github.com>
2022-04-14 20:52:33 -0500

    Postman Works!

--- controllers/denimSchemas.js
+++ controllers/denimSchemas.js
@@ -1,7 +1,6 @@
 var Denim = require('../models/denimSchema'); 
  
 // List of all Denims 
-// List of all Costumes 
 exports.denim_list = async function(req, res) { 
     try{ 
         theDenims = await Denim.find(); 
@@ -19,8 +18,24 @@ exports.denim_detail = function(req, res) {
 }; 
  
 // Handle Denim create on POST. 
-exports.denim_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Denim create POST'); 
+exports.denim_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Denim(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.length = req.body.length; 
+    document.width = req.body.width; 
+    document.stretchy = req.body.stretchy; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    } 
 }; 
  
 // Handle denim delete form on DELETE. 
@@ -31,4 +46,18 @@ exports.denim_delete = function(req, res) {
 // Handle Denim update form on PUT. 
 exports.denim_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: Denim update PUT' + req.params.id); 
+}; 
+
+
+// VIEWS 
+// Handle a show all view 
+exports.denim_view_all_Page = async function(req, res) { 
+    try{ 
+        theDenims = await Denim.find(); 
+        res.render('denim', { title: 'Denim Search Results', results: theDenims }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
--- routes/denimSchema.js
+++ routes/denimSchema.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const denim_controlers= require('../controllers/denimSchemas'); 
 var router = express.Router();
 
 /* GET home page. */
+/* GET costumes */ 
+router.get('/', denim_controlers.denim_view_all_Page ); 
+/*
 router.get('/', function(req, res, next) {
   res.render('costumeSchema', { title: 'Costume Schema' });
 });
-
+*/
 module.exports = router;
