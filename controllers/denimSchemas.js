b07d4c8c564f07352347fe1d472eccfab57cfdb1
Drake Bush <54418983+dtbush@users.noreply.github.com>
2022-04-15 11:54:39 -0500

    Lab 12

--- controllers/denimSchemas.js
+++ controllers/denimSchemas.js
@@ -11,12 +11,23 @@ exports.denim_list = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 };
- 
+/* 
 // for a specific Denim. 
 exports.denim_detail = function(req, res) { 
     res.send('NOT IMPLEMENTED: Denim detail: ' + req.params.id); 
 }; 
- 
+ */
+//for a specific Denim. 
+exports.denim_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Denim.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle Denim create on POST. 
 exports.denim_create_post = async function(req, res) { 
     console.log(req.body) 
@@ -44,10 +55,30 @@ exports.denim_delete = function(req, res) {
 }; 
  
 // Handle Denim update form on PUT. 
+/*
 exports.denim_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: Denim update PUT' + req.params.id); 
 }; 
-
+*/
+// Handle Costume update form on PUT. 
+exports.denim_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Denim.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.length) toUpdate.length = req.body.length; 
+        if(req.body.width) toUpdate.width = req.body.width; 
+        if(req.body.stretchy) toUpdate.stretchy = req.body.stretchy; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+};
 
 // VIEWS 
 // Handle a show all view 
